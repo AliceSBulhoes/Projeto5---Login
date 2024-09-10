f91a6fad1d1417a146b4e70a34d7993daacf099c
Alice <alicesb2006@gmail.com>
2024-09-10 09:41:00 -0300

    Adicionando sombra

--- src/styles/LoginStyle.jsx
+++ src/styles/LoginStyle.jsx
@@ -71,7 +71,7 @@ export const LoginStyle = styled.section`
                     /* Estilo do botão */
                     .login-btn{
                         .login-form-btn{
-                            padding: 0.5rem 1rem;
+                            padding: 0.5rem 3.5rem;
                             background-color: ${Colors.ft_color};
                             color: ${Colors.bg_color};
                             text-transform: uppercase;
@@ -81,10 +81,12 @@ export const LoginStyle = styled.section`
                             transition: ease 0.5s;
                         }
                         .login-form-btn:hover{
+                            cursor: pointer;
                             transform: scale(1.1);
                             transition: ease 0.5s;
                             background-color: ${Colors.bg_color};
                             color: ${Colors.ft_color};
+                            box-shadow: 10px 10px 10px #00000057;
                         }
                     }
 
@@ -120,6 +122,7 @@ export const LoginStyle = styled.section`
                 width: 250px;
                 border: solid 2px ${Colors.bg_color};
                 border-radius: 4px;
+                box-shadow: 10px 10px 10px #00000057;
             }
         }
     }
