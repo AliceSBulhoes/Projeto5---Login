bd9f54937cbf9f27bb8dbad193093896734ca6a5
Alice <alicesb2006@gmail.com>
2024-09-16 08:53:45 -0300

    Criando a estrutura do Cadastro

--- src/routes/Cadastrar.jsx
+++ src/routes/Cadastrar.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Link, useNavigate, useParams } from 'react-router-dom';
+import { MdCancel } from "react-icons/md";
+import logo from '../assets/logo.jpg'
 
 const Cadastrar = () => {
+    const [usuarios, setUsuaios] = useState('');
+
+
   return (
     <>
-        
+        <section className="container">
+            <div className="container-cadastro">
+            <img src={logo} alt='imagem-logo'className='img-logo'/>
+              <div className="cadastro">
+                <form className='cadastro-form'>
+                    <h1 className='titulo-cadastro'>Cadastro</h1>
+                    <div className="w-input">
+                      <input type="text" name="usuario" value={usuarios.usuario} onChange={handleChange} placeholder='Digite o seu usuário '/>
+                    </div>
+                    <div className="w-input">
+                      <input type="password" name="senha" value={usuarios.senha} onChange={handleChange} placeholder='Digite a sua senha '/>
+                    </div>
+                    <div className="cadastro-btn">
+                      <button type='submit' className="cadastro-form-btn">Cadastrar</button>
+                    </div>
+                    <Link to="/listar-usuario">
+                      <MdCancel />
+                    </Link>
+                </form>
+              </div>
+            </div>
+        </section>
     </>
   )
 }
